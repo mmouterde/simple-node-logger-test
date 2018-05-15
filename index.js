c9ea6c6e19623f586dafc2dd6f011745a8f36205
Martin Mouterde <martin.mouterde@gmail.com>
2018-05-15 22:26:15 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+    console.log(req.query);
+    res.send("");
+});
+
+app.listen(80, function () {
+    console.log('Ok')
+});
